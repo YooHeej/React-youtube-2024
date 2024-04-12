beae75b57871a100356c6c55ba0886cae3bf382b
YooHeej <sprite1139@gmail.com>
2024-04-12 16:43:53 +0900

    04121643

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,25 +1,34 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
-// import { formatAgo } from "../util/date";
+import { formatAgo } from "../util/date";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 
 
 export default function VideoCard({video}) {
   const navigate = useNavigate();
-  const {title, thumbnails, channelTitle, /*publishedAt*/} = video.snippet;
-  if (typeof(video.id) !== 'string' && video.id.kind === 'youtube#channel')
-    return;
+  const {title, thumbnails, channelTitle, publishedAt} = video.snippet;
+  // if (typeof(video.id) !== 'string' && video.id.kind === 'youtube#channel')
+  //   return;
   const videoId = typeof(video.id) === 'string' ? video.id : video.id.videoId;
-  return (  
-    <li onClick={() => {
-      navigate(`/videos/watch/${videoId}`);
-    }}>
-      <img src={thumbnails.medium.url} alt={title} />
-      <div>
-        <p>{title}</p>
-        <p>{channelTitle}</p>
-        {/* <p>{formatAgo(publishedAt, 'ko')}</p> */}
-      </div>
-    </li>
+  return (
+    <Card 
+    onClick={() => {
+      navigate(`/videos/watch/${videoId}`, {state: {video} })}}
+    >
+      <CardContent>
+        <img src={thumbnails.medium.url} alt={title} />
+        <div>
+          <Typography sx={{fontSize: 16, fontWeight: 'bold'}}>{title}</Typography>
+          <Typography>{channelTitle}</Typography>
+          <Typography>{formatAgo(publishedAt, 'ko')}</Typography>
+          {/* <p>{title}</p>
+          <p>{channelTitle}</p>
+          <p>{formatAgo(publishedAt, 'ko')}</p> */}
+        </div>
+      </CardContent>
+    </Card>
   )
 }
--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-
+import { useLocation } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import ChannelInfo from "../components/ChannelInfo";
+import RelatedVideo from "../components/RelatedVideo";
+import { Typography } from "@mui/material";
 
 export default function VideoDetail() {
+  const { state: {video} } = useLocation();
+  const { title, channelId, channelTitle, description} = video.snippet;
   return (
-    <div style={{margin: '20px'}}>
-      VideoDetail Page
-    </div>
+    <Grid container spacing={2}>
+      <Grid item xs={9} md={9}>
+        <Box sx={{paddingTop: '53%', height: 0, width: '100%', position: 'relative'}}>
+          <iframe id='player' type='text/html' width={'100%'} height={'100%'}
+            style={{position: 'absolute', top: 0, left: 0}} title={title}
+            src={`https://www.youtube.com/embed/${video.id}`} />
+        </Box>
+        <div>
+          <h3>{title}</h3>
+          <ChannelInfo id={channelId} name={channelTitle}/>
+          <pre>{description}</pre>
+        </div>
+      </Grid>
+      <Grid item xs={9} md={3}>
+        <Typography> 연관 동영상 </Typography>
+        <RelatedVideo id={channelId} name={channelTitle}/>
+      </Grid>
+    </Grid>
   )
 }
